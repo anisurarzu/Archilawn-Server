299248ad227d7e9046035ebca557d7fa810a78c8
anisurarzu <anisur35-2163@diu.edu.bd>
2024-08-11 16:56:05 +0600

    cors prblm fix

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const sliderRoutes = require("./routes/sliderRoutes"); // Import slider routes
